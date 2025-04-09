84fbdb7c3274d3b796734a07c5e66ddd2755f3c1
Grace Manning <gracebmanning@gmail.com>
2025-04-09 12:03:16 -0700

    remove filecontext provider from index

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router';
-import { FileContextProvider } from './contexts/fileContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FileContextProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </FileContextProvider>
   </React.StrictMode>
 );
