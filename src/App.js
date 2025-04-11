d2c06b0c7fd64894b69f578b00e290c587f6c403
Grace Manning <gracebmanning@gmail.com>
2025-04-10 17:05:29 -0700

    simplify routing, Editor re-rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { Route, Routes } from 'react-router';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home';
 import Editor from './pages/Editor';
-
+import FileUpload from './components/FileUpload';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<FileUpload/>} />
         <Route path="/edit" element={<Editor/>} />
       </Routes>
       <Footer/>
--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,36 +1,27 @@
-import { useLocation, useNavigate } from 'react-router';
+import { useLocation } from 'react-router';
 import AudioWaveform from '../components/AudioWaveform';
-import { useEffect, useState } from 'react';
 
 const Editor = () => {
 	console.log('Editor component rendered');
 	const location = useLocation();
-	const navigate = useNavigate();
 	const audioDataFromUpload = location.state ? location.state.audioData : null;
     const transcriptDataFromUpload = location.state ? location.state.transcriptData : null;
-	const [isAudioURLAvailable, setIsAudioURLAvailable] = useState(false);
 
-	useEffect(() => {
-        if (audioDataFromUpload) {
-            setIsAudioURLAvailable(true);
-        } else {
-            // Optionally, redirect back to the upload page if audioData is not available
-            // navigate('/');
-        }
-    }, [audioDataFromUpload, navigate]);
+	console.log('Audio Data before passing to AudioWaveform:', audioDataFromUpload);
+    console.log('Transcript Data before passing to AudioWaveform:', transcriptDataFromUpload);
 
 	return (
 		<div>
 			<h1 style={{ textAlign: 'center'}}>
 				Edit Your Audio File
 			</h1>
-			{isAudioURLAvailable && (
+			{audioDataFromUpload  && (
 				<AudioWaveform 
 					key={audioDataFromUpload}
 					audioData={audioDataFromUpload} 
 					transcriptData={transcriptDataFromUpload} />
 			)}
-			{!isAudioURLAvailable && <p style={{ textAlign: 'center' }}>Loading audio...</p>}
+			{!audioDataFromUpload && <p style={{ textAlign: 'center' }}>Loading audio...</p>}
 		</div>
 	);
 };
